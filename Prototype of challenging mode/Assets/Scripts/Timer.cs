db2782746c4857422be65fa717e2ad4c249e9d97
kanlam <kanlam1994@gmail.com>
2017-02-24 08:56:23 +0000

    24/2/2017 ( HUD Protoype with timer )

---
+++ Prototype of challenging mode/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text Timertext;
+	private float Starttime;
+	private bool finished = false;
+
+	// Use this for initialization
+	void Start () {
+		Starttime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (finished)
+			return;
+		
+		float t = Time.time - Starttime;
+
+		string hour = ((int)t / 60 / 60).ToString();
+		string minutes = ((int)t / 60).ToString();
+		string seconds = (t % 60).ToString ("f2");
+
+		Timertext.text = "Time:"+ hour + ":" + minutes + ":" + seconds;
+	}
+	public void Finish(){
+		finished = true;
+		Timertext.color = Color.cyan;
+
+	}
+}
---
+++ Prototype of challenging mode/Assets/Scripts/Win.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+	private void OnTriggerEnter (Collider other){
+		GameObject.Find ("Player").SendMessage("Finish");
+	}
+}

503978019d4edd85e0c4425981fbdf7ec0fe904a
kanlam <kanlam1994@gmail.com>
2017-03-07 02:43:59 +0000

    Start Working on Start Menu

---
+++ TurboRuuner/Assets/Scripts/BeatTheMonsterMode/WinMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinMenu : MonoBehaviour {
+
+	void Start () {
+		gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	public void ToggleWinMenu()
+	{
+		
+		gameObject.SetActive (true);
+
+	}
+
+}

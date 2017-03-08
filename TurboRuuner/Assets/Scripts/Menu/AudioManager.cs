a175e8ad3d07516c850b25b99425003601a3b0c7
kanlam <kanlam1994@gmail.com>
2017-03-08 01:01:03 +0000

    start menu prototype done

---
+++ TurboRuuner/Assets/Scripts/Menu/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public enum AudioChannel {Master, Sfx, Music};
+
+	public float masterVolumePercent = 1;
+	public float sfxVolumePercent = 1;
+	public float musicVolumePercent = 1;
+
+	AudioSource[] musicSouces;
+	int activeMusicSourceIndex;
+
+	public static AudioManager instance;
+
+	// create two audioSource at start
+	void Awake(){
+
+		instance = this;
+
+		musicSouces = new AudioSource[2];
+		for (int i = 0; i < 2; i++) {
+			GameObject newMusicSource = new GameObject ("Music source" + (i + 1));
+			musicSouces[i] = newMusicSource.AddComponent<AudioSource>();
+			newMusicSource.transform.parent = transform;
+		}
+
+		masterVolumePercent = PlayerPrefs.GetFloat ("master vol", masterVolumePercent);
+		sfxVolumePercent = PlayerPrefs.GetFloat ("sfx vol", sfxVolumePercent);
+		musicVolumePercent = PlayerPrefs.GetFloat ("music vol", musicVolumePercent);
+	}
+
+	public void SetVolume(float volumePercent, AudioChannel channel){
+		switch (channel) {
+		case AudioChannel.Master:
+			masterVolumePercent = volumePercent;
+			break;
+		case AudioChannel.Sfx:
+			sfxVolumePercent = volumePercent;
+			break;
+		case AudioChannel.Music:
+			musicVolumePercent = volumePercent;
+			break;
+		}
+		musicSouces [0].volume = musicVolumePercent * masterVolumePercent;
+		musicSouces [1].volume = musicVolumePercent * masterVolumePercent;
+
+		PlayerPrefs.SetFloat ("master vol", masterVolumePercent);
+		PlayerPrefs.SetFloat ("sfx vol", sfxVolumePercent);
+		PlayerPrefs.SetFloat ("music vol", musicVolumePercent);
+		PlayerPrefs.Save ();
+	}
+
+	public void PlayMusic(AudioClip clip, float fadeDuration =1){
+		activeMusicSourceIndex = 1 - activeMusicSourceIndex;
+		musicSouces [activeMusicSourceIndex].clip = clip;
+		musicSouces [activeMusicSourceIndex].Play ();
+
+		StartCoroutine (AnimateMusicCrossfade (fadeDuration));
+	}
+
+	public void PlayerSound(AudioClip clip, Vector3 pos){
+		if (clip != null) {
+			AudioSource.PlayClipAtPoint (clip, pos, sfxVolumePercent * masterVolumePercent);
+		}
+	}
+
+
+	IEnumerator AnimateMusicCrossfade(float duration){
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * 1 / duration;
+			musicSouces [activeMusicSourceIndex].volume = Mathf.Lerp (0, musicVolumePercent * masterVolumePercent, percent);
+			musicSouces [1-activeMusicSourceIndex].volume = Mathf.Lerp (musicVolumePercent * masterVolumePercent,0, percent);
+
+			yield return null;
+		}
+	}
+}
---
+++ TurboRuuner/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject optionMenuHolder;
+
+	public Slider[] volunmeSliders;
+	public Toggle[] resolutionTogglse;
+	public int[] screenWidths;
+
+	int activeScreenResIndex;
+
+
+	void Start(){
+		optionMenuHolder.SetActive (false);
+		activeScreenResIndex = PlayerPrefs.GetInt ("screen res index");
+		bool isFullscreen = (PlayerPrefs.GetInt ("fullscreen") == 1)?true:false; 
+
+		volunmeSliders [0].value = AudioManager.instance.masterVolumePercent;
+		volunmeSliders [1].value = AudioManager.instance.musicVolumePercent;
+		volunmeSliders [2].value = AudioManager.instance.sfxVolumePercent;
+
+		for (int i = 0; i < resolutionTogglse.Length; i++) {
+			resolutionTogglse [i].isOn = i == activeScreenResIndex;
+		}
+	
+		SetFullScreen (isFullscreen);
+	}
+
+
+
+	//Start Game Button
+	public void NewGameBtn(string BeatTheMonsterMode){
+	 SceneManager.LoadScene ("BeatTheMonsterMode", LoadSceneMode.Single);
+	}
+
+	// Quit game Button
+	public void QuitGameBtn(){
+		Application.Quit();
+	}
+
+	public void OptionMenu(){
+		optionMenuHolder.SetActive (true);
+	}
+
+	public void backToMainMenu(){
+		optionMenuHolder.SetActive (false);
+	}
+
+	public void SetScreenResolution(int i){
+		if (resolutionTogglse [i].isOn) {
+			activeScreenResIndex = i;
+			float Ratio = 16 / 9f;	
+			Screen.SetResolution (screenWidths [i], (int)(screenWidths[i]/Ratio), false);
+			PlayerPrefs.SetInt ("screen res index", activeScreenResIndex);
+			PlayerPrefs.Save ();
+		}
+	}
+	public void SetFullScreen(bool isFullscreen){
+		for (int i = 0; i < resolutionTogglse.Length; i++) {
+			resolutionTogglse [i].interactable = !isFullscreen;
+		}
+
+		if (isFullscreen) {
+			Resolution[] allResolution = Screen.resolutions;
+			Resolution maxResolution = allResolution [allResolution.Length - 1];
+			Screen.SetResolution (maxResolution.width, maxResolution.height, true);
+		} else {
+			SetScreenResolution (activeScreenResIndex);	
+		}
+		PlayerPrefs.SetInt ("fullscreen", ((isFullscreen) ? 1 : 0));
+		PlayerPrefs.Save ();
+	}
+
+	public void SetMasterVolume(float value){
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Master);
+	}
+	public void SetBGMVolume(float value){
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Music);
+	}
+	public void SetSfxVolume(float value){
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Sfx);
+	}
+
+}

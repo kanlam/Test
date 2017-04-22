8ed4674d66157c85c0e8f036a3e2da05967a74dc
kanlam <kanlam1994@gmail.com>
2017-04-22 22:00:18 +0100

    22/4/2017 Ver.

---
+++ TurboRuuner(21042017)/Assets/BGMController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+
+	public AudioSource source;
+
+	public AudioClip MainBGM;
+	public AudioClip DeadBGM;
+	public AudioClip WinBGM;
+
+	public OceanWorldBMControl character;
+
+	public bool isDead;
+	public bool isWin;
+
+	// Use this for initialization
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+		source.clip = MainBGM;
+		source.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/*void GetBool(){
+		if (character.isDead) {
+			isDead = true;
+			character.source.clip = DeadBGM;
+			character.source.Play ();
+		} else {
+			isDead = false;
+		}
+		if (character.isWin) {
+			isWin = true;
+			//source.clip = WinBGM;
+			source.PlayOneShot (WinBGM);
+		} else {
+			isWin = false;
+		}
+	}*/
+}
